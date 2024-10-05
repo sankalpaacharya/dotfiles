501d44c26cdc3b07e97c4139f4bdb09c4a22f6d6
sankalpaacharya <sankalpaacharya01@gmail.com>
2024-10-06 02:24:22 +0530

    add: polybar

---
+++ .config/polybar/scripts/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Other monitors
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
